2161a6e02b1327e47f26c0c651b38748063dc88a
SrishtiKumari07 <143869333+SrishtiKumari07@users.noreply.github.com>
2023-10-27 18:24:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculate(operation) {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const resultElement = document.getElementById('result');
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultElement.innerText = 'Please enter valid numbers';
+        return;
+    }
+
+    let result;
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                resultElement.innerText = 'Cannot divide by zero';
+                return;
+            }
+            break;
+        default:
+            result = 'Invalid operation';
+            break;
+    }
+    resultElement.innerText = `Result: ${result}`;
+
+}
